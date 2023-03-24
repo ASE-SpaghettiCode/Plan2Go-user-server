25dc7228e92a481fa3134be60429fa34a3bd8918
tluo3032 <tiantian.luo@uzh.ch>
2023-03-24 14:12:31 +0100

    Modified attribute name

--- src/main/java/ASESpaghettiCode/UserServer/Model/User.java
+++ src/main/java/ASESpaghettiCode/UserServer/Model/User.java
@@ -12,7 +12,7 @@ public class User{
     public String username;
     public String password;
     public String token;
-    public String info;
+    public String intro;
     public String imageLink;
 
     public User(String username, String password, String token){
@@ -53,12 +53,12 @@ public class User{
         this.token = token;
     }
 
-    public String getInfo(){
-        return info;
+    public String getIntro(){
+        return intro;
     }
 
-    public void setInfo(String info) {
-        this.info = info;
+    public void setIntro(String intro) {
+        this.intro = intro;
     }
 
     public void setImageLink(String imageLink) {
@@ -75,6 +75,6 @@ public class User{
                 "id='" + userId + '\'' +
                 ", username='" + username + '\'' +
                 ", password=" + password+'\''+
-                ", info=" + info +'}';
+                ", intro=" + intro +'}';
     }
 }
