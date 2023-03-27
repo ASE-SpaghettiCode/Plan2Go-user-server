f9b842e578cef7d5da1345cf120a7d5eb52a5344
HJGuan <HJGuan@users.noreply.github.com>
2023-03-27 16:02:54 +0200

    config build.gradle and .github/workflow/build.yml for CICD

--- src/main/java/ASESpaghettiCode/UserServer/UserServerApplication.java
+++ src/main/java/ASESpaghettiCode/UserServer/UserServerApplication.java
@@ -7,18 +7,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.UUID;
 
 @SpringBootApplication
 public class UserServerApplication implements CommandLineRunner {
+
 	@Autowired
 	private UserRepository repository;
 
-	public static void main(String[] args) {
+	@Bean
+	public RestTemplate getRestTemplate(){
+		return new RestTemplate();
+	}
 
+	public static void main(String[] args) {
 		SpringApplication.run(UserServerApplication.class, args);
 	}
+
 	@Override
 	public void run(String...args) throws Exception{
 		repository.deleteAll();
