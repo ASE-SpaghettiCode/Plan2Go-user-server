18640408d838170cf1d51318a77e8937a02680b8
HJGuan <HJGuan@users.noreply.github.com>
2023-03-27 15:52:56 +0200

    add restTemplate for multiserver-communication

--- src/main/java/ASESpaghettiCode/UserServer/Controller/UserController.java
+++ src/main/java/ASESpaghettiCode/UserServer/Controller/UserController.java
@@ -3,11 +3,15 @@ package ASESpaghettiCode.UserServer.Controller;
 import ASESpaghettiCode.UserServer.Model.User;
 import ASESpaghettiCode.UserServer.Repository.UserRepository;
 import ASESpaghettiCode.UserServer.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.List;
-
+import java.util.stream.Collectors;
 
 
 @RestController
@@ -68,4 +72,27 @@ public class UserController {
     public void editUser(@PathVariable("userId") String userId, @RequestBody User user){
         userService.editUser(user);
     }
+
+//    @Value("${NoteServerLocation}")
+//    private String NoteServerLocation;
+//
+//    @Autowired
+//    private RestTemplate restTemplate;
+//
+//    @RequestMapping("/{userId}/notes")
+//    @ResponseStatus(HttpStatus.OK)
+//    public List<Note> getUserNotes(@PathVariable("userId") String userId) {
+//
+//        // get users info (including basic info, list of id of published notes, list of id of liked notes) with userId
+//        List<Integer> publisedNotesIDs = Arrays.asList(101, 102, 103);//only hardcoded for the example
+//
+//        // For each noteId, call the Note Server(by default localhost:8082) and get details
+//        List<Note> publishedList;
+//        publishedList = publisedNotesIDs.stream().map(publisedNotesID->{
+//            Note note = restTemplate.getForObject(NoteServerLocation + publisedNotesID,Note.class);
+//            return note;
+//        }).collect((Collectors.toList()));
+//
+//        return  publishedList;
+//    }
 }
